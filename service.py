2bb9f4a848bb8e0bf5ad485345096db5082622b3
Omrigan <omrigann@gmail.com>
2016-07-09 22:58:27 +0700

    Changed to working service

--- service.py
+++ service.py
@@ -1,20 +1,20 @@
 from flask import Flask
-from plumbum import local
+from plumbum import local, FG
 
 
 app = Flask(__name__)
-master_directory='~oleg/test'
+directory='/home/oleg/gotosite'
 
 def run_by_dir(path):
-    local.cwd(path)
-    local['git']['pull']()
-    local['docker-compose']['up']['--build']()
+    local.cwd.chdir(path)
+    local['git']['pull'] & FG
+    local['docker-compose']['up']['--build']['-d'] & FG
 
 
 
-@app.route('goto-push')
+@app.route('/goto-push',  methods=['GET', 'POST'])
 def push_hook():
-    run_by_dir(master_directory)
-
+    run_by_dir(directory)
+    return 'OK'
 if __name__=='__main__':
     app.run()
