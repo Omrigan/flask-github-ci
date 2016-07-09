74c51dd071e5e617569499e5a54e4e7d2701187c
Omrigan <omrigann@gmail.com>
2016-07-09 22:59:36 +0700

    Added force-build

--- service.py
+++ service.py
@@ -8,7 +8,8 @@ directory='/home/oleg/gotosite'
 def run_by_dir(path):
     local.cwd.chdir(path)
     local['git']['pull'] & FG
-    local['docker-compose']['up']['--build']['-d'] & FG
+    local['docker-compose']['build']['--no-cache'] & FG
+    local['docker-compose']['up']['-d'] & FG
 
 
 
