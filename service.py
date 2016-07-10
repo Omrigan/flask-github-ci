2c58e370c2adf17767b44cfed6286a4cbfd45392
Omrigan <omrigann@gmail.com>
2016-07-10 05:52:25 -0400

    Threading

--- service.py
+++ service.py
@@ -1,21 +1,28 @@
 from flask import Flask
 from plumbum import local, FG
-
+from threading import Thread
 
 app = Flask(__name__)
 directory='/home/oleg/gotosite'
-
+execute=True
 def run_by_dir(path):
-    local.cwd.chdir(path)
-    local['git']['pull'] & FG
-    local['docker-compose']['build']['--no-cache'] & FG
-    local['docker-compose']['up']['-d'] & FG
+    global execute
+    while execute:
+        execute = False
+        local.cwd.chdir(path)
+        local['git']['pull'] & FG
+        local['docker-compose']['build']['--no-cache'] & FG
+        local['docker-compose']['up']['-d'] & FG
 
 
 
+t = Thread(target=run_by_dir, args=(directory,))
 @app.route('/goto-push',  methods=['GET', 'POST'])
 def push_hook():
-    run_by_dir(directory)
+    execute=True
+    if not t.isAlive():
+        t.start()
+    # run_by_dir(directory)
     return 'OK'
 if __name__=='__main__':
     app.run()
