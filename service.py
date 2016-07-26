a08c824beb543c49568902c860d9300783157ab3
Omrigan <omrigann@gmail.com>
2016-07-27 01:24:26 +0700

    Gs update added

--- service.py
+++ service.py
@@ -3,30 +3,37 @@ from plumbum import local, FG
 from threading import Thread
 
 app = Flask(__name__)
-directory='/home/oleg/gotosite'
-execute=True
+directory = '/home/oleg/gotosite'
+execute = True
+
+
 def run_by_dir(path):
     global execute
     while execute:
         execute = False
         local.cwd.chdir(path)
-        local['git']['pull'] & FG
-        local['docker-compose']['build'] & FG
-        local['docker-compose']['up']['-d'] & FG
-        print('Finished')
+        g = local['git']
+        dc = local['docker-compose']
+        gs = g['submodule']
+        g['pull'] & FG
+        dc['build'] & FG
+        dc['up']['-d'] & FG
+        gs['update'] & FG
+        print('Finished build')
 
 
 t = Thread(target=run_by_dir, args=(directory,))
-@app.route('/goto-push',  methods=['GET', 'POST'])
+
+
+@app.route('/goto-push', methods=['GET', 'POST'])
 def push_hook():
-    global execute
-    execute=True
-    global t
+    global execute, t
+    execute = True
     if not t.isAlive():
         t = Thread(target=run_by_dir, args=(directory,))
         t.start()
-
-    # run_by_dir(directory)
     return 'OK'
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     app.run()
